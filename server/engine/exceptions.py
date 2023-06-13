c6d05fa4a708f99755ad78cd4ce3b03e770d7f6a
Nicholas Bianco <nbianco@stanford.edu>
2023-06-13 15:15:39 -0700

    Finalize the error reporting structure

--- server/engine/exceptions.py
+++ server/engine/exceptions.py
@@ -1,17 +1,99 @@
+"""
+exceptions.py
+-------------
+Description: Custom exception classes for use in the AddBiomechanics processing engine.
+Author(s): Nicholas Bianco
+"""
+
+import textwrap
+
+
 class Error(Exception):
     """Base class for exceptions used in the AddBiomechanics engine."""
-    def __init__(self, previous_message):
-        self.previous_message = previous_message
-        self.message = f"{self.get_message()} {self.previous_message}"
+    def __init__(self, original_message):
+        self.message = f'{self.get_message()} Below is the original error message, which main contain useful ' \
+                       f'information about your issue. If you are unable to resolve the issue, please submit a ' \
+                       f'forum post at https://simtk.org/projects/addbiomechanics or a submit a GitHub Issue at ' \
+                       f'https://github.com/keenon/AddBiomechanics/issues with all error messages included.'
+        self.original_message = f'\n\n{textwrap.indent(original_message, " " * 4)}\n\n'
+        self.type = self.get_type()
 
         super().__init__(self.message)
 
     def get_message(self):
         raise NotImplementedError("Subclasses must implement the 'get_message' method.")
 
+    def get_type(self):
+        return self.__class__.__name__
+
+    def get_error_dict(self):
+        return {
+            "type": self.type,
+            "message": self.message,
+            "original_message": self.original_message
+        }
+
 
 class PathError(Error):
-    """Raised when a path is invalid."""
+    """Raised when a input data path is invalid."""
     def get_message(self):
-        return "DerivedException: This is a custom message."
+        return "PathError: Error encountered when detecting the Geometry folder, subject JSON file, and trials " \
+               "directory. These files and/or folder may be missing or invalid."
 
+
+class SubjectConfigurationError(Error):
+    """Raised when a the provided subject information is missing or malformed.."""
+    def get_message(self):
+        return "SubjectConfigurationError: Error encountered when reading in subject-specific information. Please " \
+               "check that the height, weight, sex, and model file for the subject are all provided and correct."
+
+
+class ModelFileError(Error):
+    """Raised when the provided model file is missing or malformed."""
+    def get_message(self):
+        return "ModelFileError: Error encountered when loading preset or custom model file(s). Please check that you " \
+               "have provided a valid OpenSim Model file and that the file is not corrupted (e.g., by trying to load " \
+               "it into the OpenSim GUI). If you are using a model with a custom markerset, please check that the " \
+               "markers are attached to the correct bodies and correspond to the experimental marker trajectories " \
+               "you have provided."
+
+
+class TrialPreprocessingError(Error):
+    """Raised when an error occurs during the trial segmentation step."""
+    def get_message(self):
+        return "TrialPreprocessingError: Error encountered when preprocessing the marker and/or ground " \
+               "reaction force data. Please check that your data files do not contain any NaN or other invalid " \
+               "values. It is also recommend to check that certainty quantities have zero or non-zero value when " \
+               "you expect them to be zero or non-zero, respectively. If you have provided both marker and ground " \
+               "reaction force data, please check that the number of frames in each file is the same and that the " \
+               "time stamps are consistent. If you have enabled automatic trial segmentation, try disabling this " \
+               "option and manually trimming the data for each trial."
+
+
+class MarkerFitterError(Error):
+    """Raised when an error occurs during the marker fitting step."""
+    def get_message(self):
+        return "MarkerFitterError: Error encountered when running the marker fitting step. This step is highly " \
+               "dependent on the quality of the marker data provided. Please check that the marker trajectories are " \
+               "smooth and do not contain any large gaps or other artifacts (e.g., due to filtering). Check that " \
+               "markers are labeled correctly and that the marker set is consistent across trials. Try visualizing " \
+               "the marker trajectories in the OpenSim GUI to check for any obvious issues (e.g., markers swapping " \
+               "segments at different frames)."
+
+
+class DynamicsFitterError(Error):
+    """Raised when an error occurs during the dynamics fitting step."""
+    def get_message(self):
+        return "DynamicsFitterError: Error encountered when running the dynamics fitting step. This step is highly " \
+               "dependent on the quality of the ground reaction force data provided and its consistency with the " \
+               "marker data. Please check that the ground reaction force data is smooth and does not contain any " \
+               "unexpected large gaps or other artifacts (e.g., due to filtering). Check that the force labels are " \
+               "are consistent with the standard for OpenSim ExternalLoads files. Try visualizing the ground " \
+               "reaction force data in the OpenSim GUI to check for any obvious issues (e.g., forces switching " \
+               "between feet or not aligning with the feet at all)."
+
+
+class WriteError(Error):
+    """Raised when an error occurs when writing out the results."""
+    def get_message(self):
+        return "WriteResultsError: Error encountered when writing out the result files."
--- server/engine/testing.py
+++ server/engine/testing.py
@@ -15,104 +15,105 @@ from engine import Engine
 import shutil
 import json
 from helpers import detect_nonzero_force_segments, filter_nonzero_force_segments, \
-    reconcile_markered_and_nonzero_force_segments
+                    reconcile_markered_and_nonzero_force_segments
+from exceptions import Error
 
 DATA_FOLDER_PATH = absPath('../data')
 GEOMETRY_FOLDER_PATH = absPath('Geometry')
 
 
-class TestTrialSegmentation(unittest.TestCase):
-    def test_detectNonZeroForceSegments(self):
-        # Create force plate data with intermittent loads.
-        timestamps = np.linspace(0, 6.0, 601)
-        totalLoad = np.zeros(len(timestamps))
-
-        # Add a legitimate load segment.
-        totalLoad[50:100] = 1.0
-        # Add a load segment that is too short.
-        totalLoad[250:254] = 1.0
-        # Add two load segments that should be merged together.
-        totalLoad[300:340] = 1.0
-        totalLoad[360:400] = 1.0
-        # Add a segment that has load up until the final time point.
-        totalLoad[550:601] = 1.0
-
-        # Detect the non-zero force segments.
-        nonzeroForceSegments = detect_nonzero_force_segments(timestamps, totalLoad)
-
-        # Check that the correct segments were detected, before filtering.
-        self.assertEqual(len(nonzeroForceSegments), 5)
-        self.assertEqual(nonzeroForceSegments[0], (0.5, 1.0))
-        self.assertEqual(nonzeroForceSegments[1][0], 2.5)
-        self.assertAlmostEqual(nonzeroForceSegments[1][1], 2.54, places=5)
-        self.assertEqual(nonzeroForceSegments[2], (3.0, 3.4))
-        self.assertEqual(nonzeroForceSegments[3], (3.6, 4.0))
-        self.assertEqual(nonzeroForceSegments[4], (5.5, 6.0))
-
-        # Now filter the segments.
-        nonzeroForceSegments = filter_nonzero_force_segments(nonzeroForceSegments, 0.05, 1.0)
-
-        # Check that the correct segments were detected, after filtering.
-        self.assertEqual(len(nonzeroForceSegments), 3)
-        self.assertEqual(nonzeroForceSegments[0], (0.5, 1.0))
-        self.assertEqual(nonzeroForceSegments[1], (3.0, 4.0))
-        self.assertEqual(nonzeroForceSegments[2], (5.5, 6.0))
-
-    def test_Camargo2021(self):
-        # Load the data.
-        trcFilePath = DATA_FOLDER_PATH + '/Camargo2021/levelground_ccw_fast_01_01.trc'
-        grfFilePath = DATA_FOLDER_PATH + '/Camargo2021/levelground_ccw_fast_01_01_grf.mot'
-        trcFile: nimble.biomechanics.OpenSimTRC = nimble.biomechanics.OpenSimParser.loadTRC(trcFilePath)
-        forcePlates: List[nimble.biomechanics.ForcePlate] = nimble.biomechanics.OpenSimParser.loadGRF(
-            grfFilePath, trcFile.framesPerSecond)
-
-        # Compute the total load.
-        totalLoad = np.zeros(len(forcePlates[0].timestamps))
-        for itime in range(len(totalLoad)):
-            totalForce = 0
-            totalMoment = 0
-            for forcePlate in forcePlates:
-                totalForce += np.linalg.norm(forcePlate.forces[itime])
-                totalMoment += np.linalg.norm(forcePlate.moments[itime])
-            totalLoad[itime] = totalForce + totalMoment
-
-        # Detect the non-zero force segments.
-        nonzeroForceSegments = detect_nonzero_force_segments(forcePlates[0].timestamps, totalLoad)
-
-        # Filter the segments.
-        nonzeroForceSegments = filter_nonzero_force_segments(nonzeroForceSegments, 0.05, 1.0)
-
-        # Check that the correct segments were detected.
-        self.assertEqual(len(nonzeroForceSegments), 3)
-        self.assertEqual(nonzeroForceSegments[0], (0.005, 5.92))
-        self.assertEqual(nonzeroForceSegments[1], (7.365, 8.455))
-        self.assertEqual(nonzeroForceSegments[2], (11.895, 15.335))
-
-    def test_reconcileMarkeredAndNonzeroForceSegments(self):
-        # Create force plate data with intermittent loads.
-        timestamps = np.linspace(0, 10.0, 1001)
-
-        # Create marker segments
-        markerSegments = list()
-        markerSegments.append(timestamps[0:200])
-        markerSegments.append(timestamps[500:600])
-        markerSegments.append(timestamps[800:1000])
-
-        # Create force segments
-        forceSegments = list()
-        forceSegments.append(timestamps[100:300])
-        forceSegments.append(timestamps[350:400])
-        forceSegments.append(timestamps[550:600])
-        forceSegments.append(timestamps[700:950])
-
-        # Reconcile the segments.
-        finalSegments = reconcile_markered_and_nonzero_force_segments(timestamps, markerSegments, forceSegments)
-
-        # Check that the correct segments were detected.
-        self.assertEqual(len(finalSegments), 3)
-        self.assertEqual(finalSegments[0], (1.0, 2.0))
-        self.assertEqual(finalSegments[1], (5.5, 6.0))
-        self.assertEqual(finalSegments[2], (8.0, 9.5))
+# class TestTrialSegmentation(unittest.TestCase):
+#     def test_detectNonZeroForceSegments(self):
+#         # Create force plate data with intermittent loads.
+#         timestamps = np.linspace(0, 6.0, 601)
+#         totalLoad = np.zeros(len(timestamps))
+#
+#         # Add a legitimate load segment.
+#         totalLoad[50:100] = 1.0
+#         # Add a load segment that is too short.
+#         totalLoad[250:254] = 1.0
+#         # Add two load segments that should be merged together.
+#         totalLoad[300:340] = 1.0
+#         totalLoad[360:400] = 1.0
+#         # Add a segment that has load up until the final time point.
+#         totalLoad[550:601] = 1.0
+#
+#         # Detect the non-zero force segments.
+#         nonzeroForceSegments = detect_nonzero_force_segments(timestamps, totalLoad)
+#
+#         # Check that the correct segments were detected, before filtering.
+#         self.assertEqual(len(nonzeroForceSegments), 5)
+#         self.assertEqual(nonzeroForceSegments[0], (0.5, 1.0))
+#         self.assertEqual(nonzeroForceSegments[1][0], 2.5)
+#         self.assertAlmostEqual(nonzeroForceSegments[1][1], 2.54, places=5)
+#         self.assertEqual(nonzeroForceSegments[2], (3.0, 3.4))
+#         self.assertEqual(nonzeroForceSegments[3], (3.6, 4.0))
+#         self.assertEqual(nonzeroForceSegments[4], (5.5, 6.0))
+#
+#         # Now filter the segments.
+#         nonzeroForceSegments = filter_nonzero_force_segments(nonzeroForceSegments, 0.05, 1.0)
+#
+#         # Check that the correct segments were detected, after filtering.
+#         self.assertEqual(len(nonzeroForceSegments), 3)
+#         self.assertEqual(nonzeroForceSegments[0], (0.5, 1.0))
+#         self.assertEqual(nonzeroForceSegments[1], (3.0, 4.0))
+#         self.assertEqual(nonzeroForceSegments[2], (5.5, 6.0))
+#
+#     def test_Camargo2021(self):
+#         # Load the data.
+#         trcFilePath = DATA_FOLDER_PATH + '/Camargo2021/levelground_ccw_fast_01_01.trc'
+#         grfFilePath = DATA_FOLDER_PATH + '/Camargo2021/levelground_ccw_fast_01_01_grf.mot'
+#         trcFile: nimble.biomechanics.OpenSimTRC = nimble.biomechanics.OpenSimParser.loadTRC(trcFilePath)
+#         forcePlates: List[nimble.biomechanics.ForcePlate] = nimble.biomechanics.OpenSimParser.loadGRF(
+#             grfFilePath, trcFile.framesPerSecond)
+#
+#         # Compute the total load.
+#         totalLoad = np.zeros(len(forcePlates[0].timestamps))
+#         for itime in range(len(totalLoad)):
+#             totalForce = 0
+#             totalMoment = 0
+#             for forcePlate in forcePlates:
+#                 totalForce += np.linalg.norm(forcePlate.forces[itime])
+#                 totalMoment += np.linalg.norm(forcePlate.moments[itime])
+#             totalLoad[itime] = totalForce + totalMoment
+#
+#         # Detect the non-zero force segments.
+#         nonzeroForceSegments = detect_nonzero_force_segments(forcePlates[0].timestamps, totalLoad)
+#
+#         # Filter the segments.
+#         nonzeroForceSegments = filter_nonzero_force_segments(nonzeroForceSegments, 0.05, 1.0)
+#
+#         # Check that the correct segments were detected.
+#         self.assertEqual(len(nonzeroForceSegments), 3)
+#         self.assertEqual(nonzeroForceSegments[0], (0.005, 5.92))
+#         self.assertEqual(nonzeroForceSegments[1], (7.365, 8.455))
+#         self.assertEqual(nonzeroForceSegments[2], (11.895, 15.335))
+#
+#     def test_reconcileMarkeredAndNonzeroForceSegments(self):
+#         # Create force plate data with intermittent loads.
+#         timestamps = np.linspace(0, 10.0, 1001)
+#
+#         # Create marker segments
+#         markerSegments = list()
+#         markerSegments.append(timestamps[0:200])
+#         markerSegments.append(timestamps[500:600])
+#         markerSegments.append(timestamps[800:1000])
+#
+#         # Create force segments
+#         forceSegments = list()
+#         forceSegments.append(timestamps[100:300])
+#         forceSegments.append(timestamps[350:400])
+#         forceSegments.append(timestamps[550:600])
+#         forceSegments.append(timestamps[700:950])
+#
+#         # Reconcile the segments.
+#         finalSegments = reconcile_markered_and_nonzero_force_segments(timestamps, markerSegments, forceSegments)
+#
+#         # Check that the correct segments were detected.
+#         self.assertEqual(len(finalSegments), 3)
+#         self.assertEqual(finalSegments[0], (1.0, 2.0))
+#         self.assertEqual(finalSegments[1], (5.5, 6.0))
+#         self.assertEqual(finalSegments[2], (8.0, 9.5))
 
 
 class TestRajagopal2015(unittest.TestCase):
@@ -160,17 +161,28 @@ class TestRajagopal2015(unittest.TestCase):
 
         # Run the pipeline.
         # -----------------
-        engine.validate_paths()
-        engine.parse_subject_json()
-        engine.load_model_files()
-        engine.configure_marker_fitter()
-        engine.segment_trials()
-        engine.run_marker_fitting()
-        if engine.fitDynamics:
-            engine.run_dynamics_fitting()
-        engine.write_result_files()
-        engine.generate_readme()
-        engine.create_output_folder()
+        try:
+            engine.validate_paths()
+            engine.parse_subject_json()
+            engine.load_model_files()
+            engine.configure_marker_fitter()
+            engine.preprocess_trials()
+            engine.run_marker_fitting()
+            if engine.fitDynamics:
+                engine.run_dynamics_fitting()
+            engine.write_result_files()
+            engine.generate_readme()
+            engine.create_output_folder()
+
+            # If we succeeded, write an empty JSON file.
+            with open(engine.errors_json_path, "w") as json_file:
+                json_file.write("{}")
+
+        except Error as e:
+            # If we failed, write a JSON file with the error information.
+            json_data = json.dumps(e.get_error_dict(), indent=4)
+            with open(engine.errors_json_path, "w") as json_file:
+                json_file.write(json_data)
 
         # Check the results
         # -----------------
