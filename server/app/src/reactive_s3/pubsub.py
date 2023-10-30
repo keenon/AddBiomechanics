75fea6534afad9e7b8716216287876988ce5697f
Keenon Werling <keenonwerling@gmail.com>
2023-10-30 13:11:17 -0700

    Retry the reconnection to PubSub if we get an exception when connecting

--- server/app/src/reactive_s3/pubsub.py
+++ server/app/src/reactive_s3/pubsub.py
@@ -158,8 +158,15 @@ class PubSub(PubSubSocket):
         connectFuture = self.mqttConnection.connect()
         print('Waiting for connection...')
         # Future.result() waits until a result is available
-        connectFuture.result()
-        print('Connected to PubSub')
+        result_exception = connectFuture.result()
+        if result_exception is not None:
+            print('Got exception trying to reconnect PubSub: ')
+            print(result_exception)
+            print('Will try again in 5 seconds...')
+            time.sleep(5)
+            self.connect()
+        else:
+            print('Connected to PubSub')
 
     def _message_sender(self):
         while True:
