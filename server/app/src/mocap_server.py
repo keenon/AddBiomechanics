83d727abff68639b996cbed18e117a99c745fc63
Keenon Werling <keenonwerling@gmail.com>
2023-11-07 09:20:25 -0800

    Bumping minimum requested resources to 4 CPUs and 16 GB of RAM

--- server/app/src/mocap_server.py
+++ server/app/src/mocap_server.py
@@ -723,12 +723,12 @@ class MocapServer:
                                 # Convert to MB
                                 subject_size += trial.trialSize / 1024 / 1024
 
-                            # Use 4GB of RAM per 25MB of subject data, with a minimum of 4GB and a maximum of 64GB in
+                            # Use 4GB of RAM per 25MB of subject data, with a minimum of 16GB and a maximum of 64GB in
                             # 4GB increments.
-                            mem = max(4000, min(64000, int(subject_size / 25 * 4000)))
+                            mem = max(16000, min(64000, int(subject_size / 25 * 4000)))
 
-                            # Use 1 CPU per 4GB of RAM, with a minimum of 1 CPU and a maximum of 16 CPUs.
-                            cpus = max(1, min(16, int(mem / 4000)))
+                            # Use 1 CPU per 4GB of RAM, with a minimum of 4 CPU and a maximum of 16 CPUs.
+                            cpus = max(4, min(16, int(mem / 4000)))
 
                             sbatch_command = 'sbatch -p owners --job-name ' + job_name + f' --cpus-per-task={cpus} --mem={mem}M --output=processing-%j.out --time=8:00:00 --wrap="' + \
                                 raw_command.replace('"', '\\"')+'"'
