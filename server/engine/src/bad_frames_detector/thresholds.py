Fixing minor bugs in UI output, and tweaking threshold heuristic to improve precision without sacrificing recall

server/engine/src/bad_frames_detector/thresholds.py
@@ -186,7 +186,8 @@ class ThresholdsDetector(AbstractDetector):
                                                  foot_markers: List[List[Tuple[nimble.dynamics.BodyNode, np.ndarray]]],
                                                  positions: np.ndarray,
                                                  raw_force_plate_forces: List[List[np.ndarray]],
-                                                 raw_force_plate_cops: List[List[np.ndarray]]) -> float:
+                                                 raw_force_plate_cops: List[List[np.ndarray]],
+                                                 dt: float) -> float:
         """
         Get the force-weighted convex foot CoP error for the given skeleton, foot markers, positions, and frames.
 
@@ -232,13 +233,15 @@ class ThresholdsDetector(AbstractDetector):
                         contact_forces[f] += force_mag
                 else:
                     if last_in_contact[f]:
-                        if contact_forces[f] > 100.0:
+                        if contact_forces[f] * dt > 10.0:
                             weighted_average_distances = [contact_distances[f][body] / contact_forces[f] for body in
                                                           range(num_contact_bodies)]
                             min_weighted_distance = min(weighted_average_distances)
                             if min_weighted_distance > largest_min_weighted_distance:
                                 largest_min_weighted_distance = min_weighted_distance
                             total_force += contact_forces[f]
+                        else:
+                            print(f"!! Frame {t} has a contact force of {contact_forces[f] * dt} Ns, which is less than the threshold of 10.0 Ns.")
 
                         last_in_contact[f] = False
                         contact_distances[f] = [0.0 for _ in range(num_contact_bodies)]
@@ -246,7 +249,7 @@ class ThresholdsDetector(AbstractDetector):
                     last_in_contact[f] = False
         for f in range(num_force_plates):
             if last_in_contact[f]:
-                if contact_forces[f] > 100.0:
+                if contact_forces[f] * dt > 10.0:
                     weighted_average_distances = [contact_distances[f][body] / contact_forces[f] for body in
                                                   range(num_contact_bodies)]
                     min_weighted_distance = min(weighted_average_distances)
@@ -274,7 +277,9 @@ class ThresholdsDetector(AbstractDetector):
             trial_len = subject.getTrialLength(trial)
             trial_proto = trial_protos[trial]
 
-            raw_force_plates: List[nimble.biomechanics.ForcePlate] = trial_proto.getForcePlates()
+            passes = trial_proto.getPasses()
+
+            raw_force_plates: List[nimble.biomechanics.ForcePlate] = passes[-1].getProcessedForcePlates()
             raw_force_plate_forces: List[List[np.ndarray]] = [plate.forces for plate in raw_force_plates]
             raw_force_plate_cops: List[List[np.ndarray]] = [plate.centersOfPressure for plate in raw_force_plates]
 
@@ -301,11 +306,11 @@ class ThresholdsDetector(AbstractDetector):
             # 2. Get the smoothed positions and velocities. We do this by getting the poses from the second pass, which
             # is the acceleration minimizing smoother. If the trial doesn't have this pass, we mark the entire trial as
             # excluded.
-            if len(trial_proto.getPasses()) < 2 or trial_proto.getPasses()[1].getType() != nimble.biomechanics.ProcessingPassType.ACC_MINIMIZING_FILTER:
+            if len(passes) < 2 or passes[1].getType() != nimble.biomechanics.ProcessingPassType.ACC_MINIMIZING_FILTER:
                 result.append([nimble.biomechanics.MissingGRFReason.hasInputOutliers] * trial_len)
                 continue
-            poses = trial_proto.getPasses()[1].getPoses()
-            vels = trial_proto.getPasses()[1].getVels()
+            poses = passes[1].getPoses()
+            vels = passes[1].getVels()
 
             # 3. Check if the trial has badly wrapped IK based on the smoothed velocities. In theory, the previous code
             # should prevent this from happening, but it seems in our dataset that sometimes the IK can still produce
@@ -320,7 +325,15 @@ class ThresholdsDetector(AbstractDetector):
             # markers given at the top of the file. If the CoP is outside the convex hull, we mark the frame as bad,
             # because something is probably wrong with the force plate data. Often this can be a force plate that's
             # miscalibrated in space, or someone has a bug in their CoP calculation code.
-            if self.get_force_weighted_convex_foot_cop_error(skel, foot_markers, poses, raw_force_plate_forces, raw_force_plate_cops) > 0.01:
+            dt = subject.getTrialTimestep(trial)
+            cop_foot_error = self.get_force_weighted_convex_foot_cop_error(skel,
+                                                                           foot_markers,
+                                                                           poses,
+                                                                           raw_force_plate_forces,
+                                                                           raw_force_plate_cops,
+                                                                           dt)
+            if cop_foot_error > 0.01:
+                print(f"!! Trial {trial} has a force-weighted center-of-pressure-outside-of-foot error of {cop_foot_error}m, which is higher than the threshold of 0.01m.")
                 result.append([nimble.biomechanics.MissingGRFReason.copOutsideConvexFootError] * trial_len)
                 continue
 

server/engine/src/dynamics_pass/acceleration_minimizing_pass.py
@@ -121,7 +121,8 @@ def add_acceleration_minimizing_pass(subject: nimble.biomechanics.SubjectOnDisk)
             # 4.2. Lowpass filter each non-zero segment
             for start, end in non_zero_segments:
                 # print(f"Filtering force plate {i} on non-zero range [{start}, {end}]")
-                if end - start < 10:
+                total_impulse = np.sum(np.linalg.norm(force_matrix[:, start:end], axis=0)) * dt
+                if end - start < 10 or total_impulse < 10.0:
                     # print(" - Skipping non-zero segment because it's too short. Zeroing instead")
                     for t in range(start, end):
                         force_plate_raw_forces[i][t] = np.zeros(3)

server/engine/src/writers/web_results_writer.py
@@ -33,7 +33,7 @@ def get_segment_results_json(trial_proto: nimble.biomechanics.SubjectOnDiskTrial
         'kinematicsAvgMax': np.mean(trial_passes[kinematics_pass].getMarkerMax()) if kinematics_pass != -1 else None,
         'kinematicsPerMarkerRMSE': None,
         # Dynamics fit results, if present
-        'dynamicsStatus': 'FINISHED' if dynamics_pass != -1 else 'ERROR',
+        'dynamicsStatus': 'FINISHED' if dynamics_pass != -1 else 'NOT_STARTED',
         'dynanimcsAvgRMSE': np.mean(trial_passes[dynamics_pass].getMarkerRMS()) if dynamics_pass != -1 else None,
         'dynanimcsAvgMax': np.mean(trial_passes[dynamics_pass].getMarkerMax()) if dynamics_pass != -1 else None,
         'dynanimcsPerMarkerRMSE': None,
@@ -91,20 +91,23 @@ def save_segment_to_gui(trial_proto: nimble.biomechanics.SubjectOnDiskTrial,
     if -1 < dynamics_pass_index < len(trial_passes):
         dynamics_poses = trial_passes[dynamics_pass_index].getPoses()
 
+    has_kinematics_pass = kinematics_osim is not None and kinematics_pass_index > -1 and kinematics_poses is not None
+    has_dynamics_pass = dynamics_osim is not None and dynamics_pass_index > -1 and dynamics_poses is not None
+
     # 1.1. Set up the layers
     # We want to show the markers and warnings by default if the processing steps all failed
     default_show_markers_and_warnings = True
-    if kinematics_osim is not None or dynamics_osim is not None:
+    if has_kinematics_pass or has_dynamics_pass:
         default_show_markers_and_warnings = False
     gui.createLayer(markers_layer_name, [0.5, 0.5, 0.5, 1.0], defaultShow=default_show_markers_and_warnings)
     gui.createLayer(warnings_layer_name, [1.0, 0.0, 0.0, 1.0], defaultShow=default_show_markers_and_warnings)
     gui.createLayer(force_plate_layer_name, [1.0, 0.0, 0.0, 1.0], defaultShow=True)
 
-    if kinematics_osim is not None:
+    if has_kinematics_pass:
         # Default to showing kinematics only if dynamics didn't finish
         gui.createLayer(kinematics_fit_layer_name,
-                        defaultShow=(dynamics_osim is None))
-    if dynamics_osim is not None:
+                        defaultShow=(not has_dynamics_pass))
+    if has_dynamics_pass:
         # Default to showing dynamics if it finished
         gui.createLayer(dynamics_fit_layer_name, defaultShow=True)
 
@@ -173,12 +176,12 @@ def save_segment_to_gui(trial_proto: nimble.biomechanics.SubjectOnDiskTrial,
                                width=[2.0, 1.0])
 
         # 4. Render the kinematics skeleton, if we have it
-        if kinematics_osim is not None and kinematics_pass_index > -1 and kinematics_poses is not None:
+        if has_kinematics_pass:
             kinematics_osim.skeleton.setPositions(kinematics_poses[:, t])
             gui.renderSkeleton(kinematics_osim.skeleton, prefix='kinematics_', layer=kinematics_fit_layer_name)
 
         # 5. Render the dynamics skeleton, if we have it
-        if dynamics_osim is not None and dynamics_pass_index > -1 and dynamics_poses is not None:
+        if has_dynamics_pass:
             dynamics_osim.skeleton.setPositions(dynamics_poses[:, t])
             gui.renderSkeleton(dynamics_osim.skeleton, prefix='dynamics_', layer=dynamics_fit_layer_name)
         gui.saveFrame()