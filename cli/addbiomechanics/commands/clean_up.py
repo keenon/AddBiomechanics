7e59954a7509de778e3c53e01bf3b6b82a922dad
Keenon Werling <keenonwerling@gmail.com>
2024-09-28 09:44:10 -0700

    Adding a total impulse check to the clean-up command

--- cli/addbiomechanics/commands/clean_up.py
+++ cli/addbiomechanics/commands/clean_up.py
@@ -189,7 +189,8 @@ class CleanUpCommand(AbstractCommand):
                     # 4.2. Lowpass filter each non-zero segment
                     for start, end in non_zero_segments:
                         # print(f"Filtering force plate {i} on non-zero range [{start}, {end}]")
-                        if end - start < 10:
+                        total_impulse = np.sum(np.linalg.norm(force_matrix[:, start:end], axis=0)) * dt
+                        if end - start < 10 or total_impulse < 10.0:
                             # print(" - Skipping non-zero segment because it's too short. Zeroing instead")
                             for t in range(start, end):
                                 force_plate_raw_forces[i][t] = np.zeros(3)
